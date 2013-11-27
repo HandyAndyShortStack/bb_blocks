5e5444079de4af4719d1af8e59e6a02f10e91f33
Andy Short <andynshort@gmail.com>
2013-11-27 10:31:46 -0800

    blank rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BbBlocks::Application.config.secret_token = 'cd8d4fc83099b987138647b3d8c627b28755f00f02b6845d8bb5f27b50ccdd00ef8cf84c0c2afb72e7954bab4e9c731518c2acf52324c28f09a7d3415ea20524'
