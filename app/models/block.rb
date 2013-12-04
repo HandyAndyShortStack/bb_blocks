4173a8b5d2e5574b8753d201daec823e6deb0e13
Andy Short <andynshort@gmail.com>
2013-12-04 14:14:36 -0800

    empty block subclasses now generated through metaprogramming

--- app/models/block.rb
+++ app/models/block.rb
@@ -2,4 +2,11 @@ class Block < ActiveRecord::Base
   attr_accessible :options, :sandbox_id, :type
   belongs_to :sandbox
   serialize :options, Hash
+
+  def self.create_child_models
+    child_models = ['HTMLBlock', 'BlueSquareBlock', 'RedSquareBlock']
+    child_models.each do |model_name|
+      Object.const_set model_name, Class.new(Block)
+    end
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,9 @@ module BbBlocks
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.after_initialize do
+      Block.create_child_models
+    end
   end
 end
